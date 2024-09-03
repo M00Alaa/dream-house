ca0ae7774afecad4bea967c75dff0cd309a2cbd5
M00Alaa <mohamedalaa.ng@gmail.com>
2024-09-03 07:05:21 +0300

    finish prob component

--- src/app/pages/prob/prob.component.ts
+++ src/app/pages/prob/prob.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-prob',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./prob.component.scss']
 })
 export class ProbComponent {
+  selectedValue: number = 0;
 
+  constructor(private router: Router) { }
+
+  selectCard(value: number) {
+    this.selectedValue = value;
+    // console.log(this.selectedValue);
+    // this.router.navigate(['/pre-step']);
+  }
+
+  createProject() {
+    console.log(this.selectedValue);
+    this.router.navigate(['/submission']);
+  }
 }
